293cb3eebdffea096ba18b2568e6db328a91e9c0
jbracho <josejbracho@gmail.com>
2021-07-12 18:42:18 -0400

    First commit

---
+++ problems/01-keys-in-object.js
@@ -0,0 +1,35 @@
+/***********************************************************************
+Write a function called `keysInObject(obj)` that takes in an object and returns 
+an array of all the keys within that Object.
+
+Do this once using using a `for...in` loop and once using `Object.keys`.
+
+Examples:
+
+let animals = {dog: 'Wolfie', cat: 'Jet', bison: 'Bilbo'}
+let foods = {apple: 'tart', lemon: 'sour', mango: 'sweet'}
+keysInObject(animals); // => ["dog", "cat", "bison"]
+keysInObject(foods); // => ["apple", "lemon", "mango"]
+***********************************************************************/
+
+//'for...in' lopp solution
+
+//function keysInObject(obj) {
+//  let keysArr = [];
+//
+//  for (keys in obj) {
+//    keysArr.push(keys);
+//  }
+//
+//  return keysArr;
+//}
+
+//'Object.keys' solution
+
+function keysInObject(obj) {
+  return Object.keys(obj); 
+};
+
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = keysInObject;
---
+++ problems/10-rest-sum.js
@@ -0,0 +1,24 @@
+/***********************************************************************
+Write a function named `restSum` that accepts all incoming parameters and sums them.
+
+**Hint**: Use rest parameter syntax!
+
+Examples:
+restSum(3,5,6); // => 14
+restSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 14
+restSum(0); // => 0
+***********************************************************************/
+
+// you'll need to change the parameters!
+function restSum(...num) {
+  let sum = 0
+  
+  num.forEach(function(n) {
+    sum += n;
+  });
+
+  return sum;
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = restSum;
